e27a521a8f011183c7b500f5c4abbccbcb7c88db
AnakinRM <83851136+AnakinRM@users.noreply.github.com>
2024-11-15 23:44:20 -0500

    Exchange Day 4 again
    ~try  to make ethereum transaction
    ~create User, which contain order ID
    ~improve the place limit order. when place limit, user transfer the ETH to the exchange
    ~improve handle market order

--- orderbook/orderbook.go
+++ orderbook/orderbook.go
@@ -18,6 +18,7 @@ type Match struct {
 // Order from the users
 type Order struct {
 	ID        int64
+	UserID    int64
 	Size      float64 //How many BTC
 	Bid       bool    //buy or sell, true is buy, false is sell
 	Limit     *Limit  // track which limit the order in
@@ -30,9 +31,10 @@ func (o Orders) Len() int           { return len(o) }
 func (o Orders) Swap(i, j int)      { o[i], o[j] = o[j], o[i] }
 func (o Orders) Less(i, j int) bool { return o[i].Timestamp < o[j].Timestamp }
 
-func NewOrder(bid bool, size float64) *Order {
+func NewOrder(bid bool, size float64, userID int64) *Order {
 	return &Order{
 		ID:        int64(rand.Intn(10000000)),
+		UserID:    userID,
 		Size:      size,
 		Bid:       bid,
 		Timestamp: time.Now().UnixNano(),
